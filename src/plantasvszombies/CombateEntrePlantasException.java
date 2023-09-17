03abce8a449dff26b259fa3f419917aa64b6a78e
nmirgar <nuriamirgar@gmail.com>
2023-09-17 21:28:54 +0200

    modificacion

---
+++ src/plantasvszombies/CombateEntrePlantasException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantasvszombies;
+
+/**
+ *
+ * @author eps
+ */
+public class CombateEntrePlantasException extends Exception {
+    
+    public CombateEntrePlantasException(String mensaje) {
+        super(mensaje);
+    }
+  
+}
---
+++ src/plantasvszombies/CombateEntreRecolectorasException.java
@@ -0,0 +1,13 @@
+
+package plantasvszombies;
+
+/**
+ *
+ * @author nuria
+ */
+public class CombateEntreRecolectorasException extends Exception{
+    
+    public CombateEntreRecolectorasException(String mensaje) {
+        super(mensaje);
+    }
+}
